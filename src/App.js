8c9cca80fa19c0fab4b2d32016c863ff1e1d995d
Goodarzyar <goodarzyar@ifb.ir>
2022-09-11 14:28:01 +0430

    84.adding header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
 import  { Route ,Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.components";
 const HatsPage = () => (
   <div>
     <h1>
@@ -14,6 +14,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+       <Header></Header>
 
      <Switch>
 
