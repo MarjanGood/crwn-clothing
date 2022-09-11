56d42bad4375a5f5459b40ed3b1c24abff5ae968
Goodarzyar <goodarzyar@ifb.ir>
2022-09-11 15:44:20 +0430

    87.create signin form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import  { Route ,Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.components";
+
+import SignInAndSignUp from './pages/signIn-and-signUp/signIn-and-signUp.components'
 const HatsPage = () => (
   <div>
     <h1>
@@ -14,12 +16,13 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-       <Header></Header>
+     <Header></Header>
 
      <Switch>
 
        <Route exact path='/' component={HomePage}></Route>
-       <Route  path='/shop' component={ShopPage}></Route>
+       <Route path='/shop' component={ShopPage}></Route>
+       <Route path='/signin' component={SignInAndSignUp}></Route>
 
     </Switch>
     </div>
---
+++ src/pages/signIn-and-signUp/signIn-and-signUp.components.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './signIn-and-signUp.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.components'
+
+const SignInAndSignUp =()=>(
+
+<div className="signIn-and-signUp">
+   signIn-and-signUp
+   <SignIn></SignIn>
+</div>
+
+)
+export default SignInAndSignUp;
+
